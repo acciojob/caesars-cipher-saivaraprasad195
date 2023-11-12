1571d7e45179bf4098a75f59c9aaf147cc1a7f79
saivaraprasad195 <62549091+saivaraprasad195@users.noreply.github.com>
2023-11-12 18:06:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
 // Your Script here.
-
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    "?": "?",
+    ",": ",",
+  };
+  
+  function rot13(encodedStr) {
+    let decodedArr = []; // Your Result goes here
+      let n = encodedStr.length;
+      for(let i=0;i<n;i++){
+        let charCode = encodedStr.charCodeAt(i);
+        if(charCode >= 65 && charCode <= 90){
+            charCode += 13;
+            decodedArr[i] = String.fromCharCode(charCode);
+        }
+      }
+      
+  
+    return decodedArr; 
+  }
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	let n = encodedStr.length;
-	let result = "";
-	for(let i=0;i<n;i++){
-		let charCode = encodedStr[i]
-	}
-	
-
-  return decodedArr; //return decodedArr
-}
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-console.log(rot13("SERR YBIR? NPPVBWBO"));
-
+  console.log(rot13("ASERR YBIR? NPPVBWBO"));
 // Do not change this line
 window.rot13 = rot13;
