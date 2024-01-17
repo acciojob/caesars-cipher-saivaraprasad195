a26793ea97252c74828b7291af45cc59239c63ab
saivaraprasad195 <62549091+saivaraprasad195@users.noreply.github.com>
2024-01-17 22:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 // Your Script here.
-const lookup = {
-    A: "N",
-    B: "O",
-    C: "P",
-    D: "Q",
-    E: "R",
-    F: "S",
-    G: "T",
-    H: "U",
-    I: "V",
-    J: "W",
-    K: "X",
-    L: "Y",
-    M: "Z",
-    N: "A",
-    O: "B",
-    P: "C",
-    Q: "D",
-    R: "E",
-    S: "F",
-    T: "G",
-    U: "H",
-    V: "I",
-    W: "J",
-    X: "K",
-    Y: "L",
-    Z: "M",
-    "?": "?",
-    ",": ",",
-  };
+// const lookup = {
+//     A: "N",
+//     B: "O",
+//     C: "P",
+//     D: "Q",
+//     E: "R",
+//     F: "S",
+//     G: "T",
+//     H: "U",
+//     I: "V",
+//     J: "W",
+//     K: "X",
+//     L: "Y",
+//     M: "Z",
+//     N: "A",
+//     O: "B",
+//     P: "C",
+//     Q: "D",
+//     R: "E",
+//     S: "F",
+//     T: "G",
+//     U: "H",
+//     V: "I",
+//     W: "J",
+//     X: "K",
+//     Y: "L",
+//     Z: "M",
+//     "?": "?",
+//     ",": ",",
+//   };
   
   function rot13(encodedStr) {
     let decodedArr = []; // Your Result goes here
@@ -50,6 +50,6 @@ const lookup = {
     return decodedArr.toString(); 
   }
 
-  // console.log(rot13("SERR YBIR? NPPVBWBO"));
+  console.log(rot13("SERR YBIR? NPPVBWBO"));
 // Do not change this line
 window.rot13 = rot13;
